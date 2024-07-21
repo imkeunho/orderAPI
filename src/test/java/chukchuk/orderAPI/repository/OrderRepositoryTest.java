a08d7fbd544aa518a485c559d1311a26cfebf4f0
imkeunho@naver.com <imkeunho@naver.com>
2024-07-22 00:38:47 +0900

    account 누락 수정

--- src/test/java/chukchuk/orderAPI/repository/OrderRepositoryTest.java
+++ src/test/java/chukchuk/orderAPI/repository/OrderRepositoryTest.java
@@ -1,24 +1,10 @@
 package chukchuk.orderAPI.repository;
 
-import chukchuk.orderAPI.domain.Order;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
 public class OrderRepositoryTest {
 
-    private OrderRepository orderRepository;
-
-    @Test
-    void getOrder() {
-
-        Optional<Order> result = orderRepository.findById(1L);
-        Order order = result.orElseThrow();
-
-
-    }
 }
